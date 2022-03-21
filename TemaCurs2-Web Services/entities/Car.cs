b93f17ba8cb5327b549c6c87a368288587ef1bba
IlincaB <101392632+IlincaB@users.noreply.github.com>
2022-03-21 14:37:14 +0200

    Create Car.cs

---
+++ TemaCurs2-Web Services/entities/Car.cs
@@ -0,0 +1,10 @@
+namespace TemaCurs2.entities
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsAvailable { get; set; }
+        public int  CustomerId { get; set; }
+    }
+}
